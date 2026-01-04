a1dcd07ece332f0bc46e917fae85813ee38c3851
ironssam <3482442+ironssam@users.noreply.github.com>
2026-01-05 09:18:50 +1100

    tweaks to euclidean rhythm study

--- lo_fi_2.js
+++ lo_fi_2.js
@@ -13,7 +13,7 @@ sound("sd(4,8)")
 
 sound("rim(3,16)")
   .bank("RolandTR707")
-  .every(4, x => x.fast(2))
+  .every(3, x => x.fast(2))
   .gain(rand.range(.7, .8)),
 
 sound("hh(4,16)")
@@ -21,29 +21,33 @@ sound("hh(4,16)")
   .gain(rand.range(.7, .8))
 ).lpf(sine.range(500, 1000))
 
-let chords = stack (
-note (`<
-    [d2, f3, a3, c3]
-    [g, b, d, f]
-    [c, e, g, b]
-    [c3, e3, g3, c4]
-    >`)
-    .sound("piano")
-    .gain(rand.range(.8, .9))
-  ).add(note(perlin.range(0,.2)))
+let chords = note (`<
+  [d2, c3, f3, b3, e4]
+  [g3, f4, a4, b4, e5]
+  [c3, b3, d4, e4, a4]
+  [c3, b3, e4, a4, d5]
+  >`)
+  .sound("piano")
+  .gain(rand.range(.8, .9))
+  .add(note(perlin.range(0,.2)))
   .room(.5)
   .delay(.1)
   .clip(2)
 
-let bass = stack (
-note(`<
-     [d2@2 d3 [e3 f3]] 
-     [[g2@3 f2] [g3@3 f3]] 
-     [e2 c3] [[c3@3 g2] 
-     [c3@3 g2]]>`)
-     .sound("gm_acoustic_bass")
-).add(note(perlin.range(0,.2)))
+let bass = note(`<
+  [d2@2 d3 [e3 f3]] 
+  [[g2@3 f2] [g3@3 f3]] 
+  [e2 c3] [[c3@3 g2] 
+  [c3@3 g2]]>`)
+  .sound("gm_acoustic_bass")
+  .add(note(perlin.range(0,.2)))
+  .cutoff(sine.segment(4).range(200,800))
 
-$: stack (chords, bass, drums)
+let sub = note("[d2 g1 c2 c2]/4")
+  .sound(sine)
+  .lpf(sine.range(850, 1500))
+  .cutoff(sine.segment(4).range(200,800))
+
+$: stack (drums, chords, bass, sub)
 $: sound("<pink>/2").gain(.1)
 $: sound("crackle").gain(1)
